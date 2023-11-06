7a139c853b13c342587f0b9462b29dad6f0f6e9a
Amós Batista <amos.silva@gmail.com>
2023-11-06 18:44:14 -0300

    chore: melhorias de log

--- src/Runner.ts
+++ src/Runner.ts
@@ -3,12 +3,13 @@ class Runner {
   interval: any;
 
   constructor(
-    private secondsToAnother: number) {
+    private secondsToAnother: number,
+    private runnerName: string) {
     this.canRunAnotherProcess = true;
   }
 
   Init(toExecute:any ) {
-
+    console.log(`Iniciando novo processamento de Runner sob Nome ${this.runnerName} em ${this.secondsToAnother} segundos.` )
     this.interval = setInterval(() => {
       if(this.canRunAnotherProcess == true){
         this.canRunAnotherProcess = false;
