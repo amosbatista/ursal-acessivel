c8a54f27dbdb17f5fc88d8ed9e7c4ba8f74f391f
Cadu Silva <contato@cadusilva.com>
2023-11-18 16:31:42 -0300

    Implementação de novas variáveis de ambiente e ajustes

--- src/status/Status.spec.ts
+++ src/status/Status.spec.ts
@@ -22,7 +22,7 @@ describe('Status test', () => {
 
 
     const expected: IStatus = {
-      status: `Alto lá! :policia: \n\nCom licença, @${post.user.userName}.\n\nReparei que este post (${post.url}) tem uma imagem ou vídeo sem descrição. Como na ${process.env.INSTANCE_NAME} temos muito apreço por abrir as portas e incluir pessoas com deficiência visual, gostaria de reforçar com você este acordo de acessibilidade.\n\nPor favor, veja se é possível revisar e editar seu toot (via app ou web), adicionando descrição nas mídias.\n\nMuito obrigado por sua compreensão e apoio!`
+      status: `${process.env.DM_MSG}`
     }
 
     expect(CreateStatusForNonAcessiblePost(post)).toEqual(expected);
